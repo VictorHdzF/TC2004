dc250d88beb6a4dfb1489fda2ae91a02e6b3c1bc
Victor Hernandez <victor_hferzuli@hotmail.com>
2017-02-01 15:47:41 -0600

    Update TimeStamp.cpp

--- TimeStamp.cpp
+++ TimeStamp.cpp
@@ -1,14 +1,41 @@
 #include <iostream>
+#include <chrono>
 #include <ctime>
 #include <string>
-
 using namespace std;
 
-void main()
+class timeStamp{
+private:
+	static timeStamp *inst;
+
+public:
+	timeStamp()
+	{
+	
+	}
+
+	static timeStamp *instance();
+
+	virtual string getValue()
+	{
+		static time_t timeNow;
+		time(&timeNow);
+		return (ctime(&timeNow));
+	}
+};
+
+timeStamp *timeStamp::inst = 0;
+
+timeStamp *timeStamp::instance()
 {
-	static time_t timeNow;
+	if (!inst)
+		inst = new timeStamp();
 
-	time(&timeNow);
+	return inst;
+}
 
-	cout << ctime(&timeNow) << endl;
+int main()
+{
+	cout << "El tiempo es: " << timeStamp::instance()->getValue() << endl;
+	return 0;
 }
