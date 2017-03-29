3ae50786b848440ae0ec02ab51abd3d4476ad379
Victor Hernandez <victor_hferzuli@hotmail.com>
2017-03-28 22:01:13 -0600

    Update visitorAfter.cpp

--- Visitor/visitorAfter.cpp
+++ Visitor/visitorAfter.cpp
@@ -42,29 +42,28 @@ int Double::countDouble = 0;
 template<class T>
 class Visitor
 {
-
 public:
+	Visitor() { inst = this; }
+	static Visitor* inst;
+
 	virtual void visit(Integer*) = 0;
 	virtual void visit(Double*) = 0;
+
+	static Visitor* getInstance() 
+	{
+		if (inst != 0)
+			return inst;
+		else
+		{
+			Visitor();
+			return inst;
+		}
+	}
 };
 
 class PresentVisitor : public Visitor<class T>
 {
-	PresentVisitor()
-	{
-		instance = this;
-	}
-	static PresentVisitor* instance;
 public:
-	static PresentVisitor* getInstance()
-	{
-		if (instance == 0)
-		{
-			return new PresentVisitor;
-		}
-		else
-			return instance;
-	}
 
 	void visit(Integer* i)
 	{
@@ -75,7 +74,6 @@ public:
 		cout << d->getQuienSoy();
 	}
 };
-PresentVisitor* PresentVisitor::instance = 0;
 
 class CountVisitor : public Visitor<class T>
 {
@@ -116,9 +114,11 @@ void Integer::accept(Visitor<T>* v)
 
 int main()
 {
-	AddVisitor a;
+	/*AddVisitor a;
 	CountVisitor c;
-	PresentVisitor* p = PresentVisitor::getInstance();
+	PresentVisitor* p = PresentVisitor::getInstance();*/
+
+	Visitor<AddVisitor>* add = Visitor<AddVisitor>::getInstance();
 
 	Number<T>* n[] = { new Integer, new Double};
 
@@ -126,13 +126,13 @@ int main()
 	{
 		for (int j = 0; j < 2; j++)
 		{
-			n[j]->accept(&a);
+			n[j]->accept(add);
 		}
 	}
 
-	n[0]->accept(p);
+	/*n[0]->accept(p);
 	n[0]->accept(&c);
 	cout << endl;
 	n[1]->accept(p);
-	n[1]->accept(&c);
+	n[1]->accept(&c);*/
 }
